477a3b0dae9e9f22e085b8f0ef2abe02f1ccbc8e
cesarapires <cesar.augusto.pires@hotmail.com>
2021-04-19 23:03:37 -0300

    Criação do Banco e também das tabelas relacionadas ao produto

---
+++ database/migrations/2021_04_20_014803_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->char('name',30);
+            $table->char('id_type',20);
+            $table->char('id_size',20);
+            $table->float('price_buy');
+            $table->float('price_sell');
+            $table->date('date_buy');
+            $table->char('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
